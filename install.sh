a6985e3b1ffdede37b73ee150c0c23e7f52f519e
Justin Zhao <justin-zhao@qq.com>
2015-09-02 14:31:20 +0800

    Setup environment for new machine
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo 'The "~/bin/prjsetup.sh" and "~/.vim" will be removed.'
+read -p "Are you sure to setup environment for $USER(yes/no):" confirmr
+
+if [ "$confirmr" = "yes" -o "$confirmr" = "y" -o "$confirmr" = "Y" -o "$confirmr" = "YES" ]; then
+	sudo rm -rf ~/bin/prjsetup.sh
+	sudo rm -rf ~/.vim
+
+	sudo apt-get install -y exuberant-ctags cscope ckermit vim
+
+	mkdir -p ~/bin
+	cp -r bin/* ~/bin/
+	cp -r vim ~/.vim
+	cp kermrc ~/.kermrc
+
+	sudo chown -R $USER: ~/bin
+	sudo chown -R $USER: ~/.vim
+
+	searchr=`grep 'export PATH=\$PATH:~/bin' ~/.bashrc`
+	if [ "$searchr" = "" ]; then
+		echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+	fi
+else
+	echo "The install is canceled."
+fi
