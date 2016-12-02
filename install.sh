575a3735b5ed2785129dc27f579a6179877d2aa1
Justin Zhao <justin-zhao@qq.com>
2016-12-02 15:18:19 +0800

    Add minicom for CentOS
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

--- install.sh
+++ install.sh
@@ -3,31 +3,42 @@
 echo 'The "~/bin/prjsetup.sh" and "~/.vim" will be removed.'
 read -p "Are you sure to setup environment for $USER(yes/no):" confirmr
 
-if [ "$confirmr" = "yes" -o "$confirmr" = "y" -o "$confirmr" = "Y" -o "$confirmr" = "YES" ]; then
-	sudo rm -rf ~/bin/prjsetup.sh
-	sudo rm -rf ~/.vim
-
-	sudo apt-get install -y exuberant-ctags cscope gkermit vim screen
-
-	mkdir -p ~/bin
-	cp -r bin/* ~/bin/
-	cp -r vim ~/.vim
-	cp kermrc ~/.kermrc
-
-	sudo chown -R $USER: ~/bin
-	sudo chown -R $USER: ~/.vim
-
-	searchr=`grep 'export PATH=\$PATH:~/bin' ~/.bashrc`
-	if [ "$searchr" = "" ]; then
-		echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
-	fi
-
-	searchr=`grep 'caption always' /etc/screenrc`
-	if [ "$searchr" = "" ]; then
-		sudo sed -i '$acaption always "%{.bW}%-w%{.rW}%n %t%{-}%+w %=%H %Y/%m/%d "' /etc/screenrc
-	fi
-
-	sed -i 's/^function! s:handleMiddleMouse()/&\n\treturn/g' ~/.vim/plugin/NERD_tree.vim
-else
+if [ "$confirmr" != "yes" -a "$confirmr" != "y" -a "$confirmr" != "Y" -a "$confirmr" != "YES" ]; then
 	echo "The install is canceled."
+	exit 0
 fi
+
+#Clear old data
+sudo rm -rf ~/bin/prjsetup.sh
+sudo rm -rf ~/.vim
+
+#Check distribution version
+apt-get -h 2>/dev/null
+distrover=$?
+
+if [ "$distrover"x = "0"x ]; then
+	sudo apt-get install -y exuberant-ctags cscope gkermit vim screen
+else
+	sudo yum install -y ctags cscope vim screen minicom
+fi
+exit 0
+
+mkdir -p ~/bin
+cp -r bin/* ~/bin/
+cp -r vim ~/.vim
+cp kermrc ~/.kermrc
+
+sudo chown -R $USER: ~/bin
+sudo chown -R $USER: ~/.vim
+
+searchr=`grep 'export PATH=\$PATH:~/bin' ~/.bashrc`
+if [ "$searchr" = "" ]; then
+	echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+fi
+
+searchr=`grep 'caption always' /etc/screenrc`
+if [ "$searchr" = "" ]; then
+	sudo sed -i '$acaption always "%{.bW}%-w%{.rW}%n %t%{-}%+w %=%H %Y/%m/%d "' /etc/screenrc
+fi
+
+sed -i 's/^function! s:handleMiddleMouse()/&\n\treturn/g' ~/.vim/plugin/NERD_tree.vim
