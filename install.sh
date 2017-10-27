cf5b3d1284a2edd26ca3d44d4147e14af310239d
Justin Zhao <justin-zhao@qq.com>
2017-10-27 16:25:39 +0800

    Replace tab as blank space
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo 'The "~/bin/prjsetup.sh" and "~/.vim" will be removed.'
 read -p "Are you sure to setup environment for $USER(yes/no):" confirmr
 
 if [ "$confirmr" != "yes" -a "$confirmr" != "y" -a "$confirmr" != "Y" -a "$confirmr" != "YES" ]; then
-	echo "The install is canceled."
-	exit 0
+    echo "The install is canceled."
+    exit 0
 fi
 
 #Clear old data
@@ -17,9 +17,9 @@ apt-get -h 2>/dev/null
 distrover=$?
 
 if [ "$distrover"x = "0"x ]; then
-	sudo apt-get install -y exuberant-ctags cscope gkermit vim screen
+    sudo apt-get install -y exuberant-ctags cscope gkermit vim screen
 else
-	sudo yum install -y ctags cscope vim screen minicom
+    sudo yum install -y ctags cscope vim screen minicom
 fi
 
 #Config the app
@@ -33,16 +33,16 @@ sudo chown -R $USER: ~/.vim
 
 searchr=`grep 'export PATH=\$PATH:~/bin' ~/.bashrc`
 if [ "$searchr" = "" ]; then
-	echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+    echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
 fi
 
 searchr=`grep 'caption always' /etc/screenrc`
 if [ "$searchr" = "" ]; then
-	sudo sed -i '$acaption always "%{.bW}%-w%{.rW}%n %t%{-}%+w %=%H %Y/%m/%d "' /etc/screenrc
+    sudo sed -i '$acaption always "%{.bW}%-w%{.rW}%n %t%{-}%+w %=%H %Y/%m/%d "' /etc/screenrc
 fi
 
 if [ "$distrover"x != "0"x ]; then
-	sed -i 's/[^"]cs add .\//"cs add .\//g' ~/.vim/vimrc
+    sed -i 's/[^"]cs add .\//"cs add .\//g' ~/.vim/vimrc
 fi
 sed -i 's/^function! s:handleMiddleMouse()/&\n\treturn/g' ~/.vim/plugin/NERD_tree.vim
 
